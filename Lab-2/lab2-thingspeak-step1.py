a2b0aba186cfe36925ca639694f707474c18a125
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-10-01 11:06:45 -0400

    feat: Make lab2-thingspeak-step1 more efficient

--- Lab-2/lab2-thingspeak-step1.py
+++ Lab-2/lab2-thingspeak-step1.py
@@ -2,23 +2,30 @@ import http.client
 import urllib.parse
 import time
 key = "classified"
-def thermometer():
+def thermometer(temp_fd):
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = http.client.HTTPConnection("api.thingspeak.com:80")
     while True:
         #Calculate CPU temperature of Raspberry Pi in Degrees C
-        temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
+        temp_fd.seek(0)
+        temp = int(temp_fd.read()) / 1e3 # Get Raspberry Pi CPU temp
         params = urllib.parse.urlencode({'field1': temp, 'key':key })
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = http.client.HTTPConnection("api.thingspeak.com:80")
         try:
             conn.request("POST", "/update", params, headers)
             response = conn.getresponse()
             print(temp)
             print(response.status, response.reason)
             data = response.read()
-            conn.close()
         except:
-            print("connection failed")
-        break
+            try:
+                print("Closing connection")
+                conn.close()
+            finally:
+                break
+
 if __name__ == "__main__":
-        while True:
-                thermometer()
+    temp_fd = open('/sys/class/thermal/thermal_zone0/temp')
+    try:
+        thermometer(temp_fd)
+    finally:
+        temp_fd.close()
