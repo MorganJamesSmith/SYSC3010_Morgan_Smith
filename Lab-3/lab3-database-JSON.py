7a6dc956ea65b5d1ab332069a3a468f5c092a82f
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-10-01 10:44:39 -0400

    feat: Complete lab 3

--- Lab-3/lab3-database-JSON.py
+++ Lab-3/lab3-database-JSON.py
@@ -19,31 +19,22 @@ def database_disconnect():
 
 def database_create_table():
     weather_table = """ CREATE TABLE IF NOT EXISTS weather (
-                            tdate DATE NOT NULL,
-                            ttime TIME NOT NULL,
+                            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
                             city TEXT NOT NULL,
                             temp REAL NOT NULL,
                             humidity INTEGER NOT NULL,
                             pressure INTEGER NOT NULL,
                             windspeed REAL NOT NULL
                                         ); """
-    try:
-        c = conn.cursor()
-        c.execute(weather_table)
-    except Error as e:
-        print(e)
+    with conn:
+        conn.execute(weather_table)
 
 def database_insert_weather(values):
-    try:
-        c = conn.cursor()
-        c.execute('''INSERT INTO weather VALUES(date('now'), time('now'), ?, ?, ?, ?, ?)''', values)
-    except Error as e:
-        print(e)
+    with conn:
+        conn.execute('''INSERT INTO weather (city, temp, humidity, pressure, windspeed) VALUES(?, ?, ?, ?, ?)''', values)
 
 def database_fetch_weather():
-        c = conn.cursor()
-        c.execute('''SELECT * FROM weather''')
-        return c
+        return conn.execute('''SELECT temp, humidity, pressure, windspeed FROM weather ORDER BY timestamp DESC LIMIT 1''')
 
 
 def obtain_weather_data(city):
@@ -83,22 +74,14 @@ database_connect("weather.db")
 database_create_table()
 values = (data["name"], data["main"]["temp"], data["main"]["humidity"], data["main"]["pressure"], data["wind"]["speed"])
 database_insert_weather(values)
-conn.commit()
-
-db_data = database_fetch_weather();
-for row in db_data:
-    print(row)
-
-# So I've gotten the data into the database, but now I need to figure
-# out how to select only the latest date, and how to actually get the
-# data out of the cursor
-
-database_disconnect()
-
 
 degreeSym = chr(176)
+db_data = database_fetch_weather();
+for row in db_data:
+    print ("Temperature: %d%sF" % (row[0], degreeSym))
+    print ("Humidity: %d%%" % row[1])
+    print ("Pressure: %d" % row[2])
+    print ("Wind : %d" % row[3])
 
-print ("Temperature: %d%sF" % (data["main"]["temp"], degreeSym ))
-print ("Humidity: %d%%" % data["main"]["humidity"])
-print ("Pressure: %d" % data["main"]["pressure"] )
-print ("Wind : %d" % data["wind"]["speed"])
+
+database_disconnect()
